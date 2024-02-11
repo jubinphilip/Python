a14c3652f7752ad9c39e55387ba4d1a14ee2d0a1
Jubin Philip <83642952+jubinphilip@users.noreply.github.com>
2024-02-11 15:46:03 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,17 +1,17 @@
 import math
 
-n=int(input("Enter number2 :"))
+n=int(input("Enter number1 :"))
 
-m=int(input("Enter number1 :"))
+m=int(input("Enter number2 :"))
 
 def top2():
 	print("\n")
 	for i in range(math.ceil(m/2)):
-		print("\_/",end='   ')
+		print("\___/",end='   ')
 			
 	print("\n",end='/')
 	for j in range(int(m/2)):
-		print("   ",end='\_/')
+		print("   ",end='\___/')
 	print("\033[F",end="")
 for i in range(n-1):
 	top2()
